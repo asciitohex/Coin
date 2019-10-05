3f084472dd7a8764fafed4158ebc9dbcc9b613bb
ber34 <ber34@o2.pl>
2019-10-05 19:06:08 +0200

    Update WalletAcontAdresClass.php

--- class/WalletAcontAdresClass.php
+++ class/WalletAcontAdresClass.php
@@ -1,1 +1,25 @@
 <?php
+include_once ("./class/WalletClass.php");
+
+class WalletAcont{
+
+// Dołączamy do Wallet
+	// public $seed;
+	public $soll;
+	public $walletAdres;
+	
+	public function __construct($seed){
+		
+		// klucz publiczny i klucz prywatny
+		    $wallet = new Wallet();
+
+		    $wallet->setCreateAdresWallet($seed);
+			
+		   $this->walletAdres = $wallet->getAdresWallet();
+		   //$this->seed = $seed;
+		   
+		   // klucz publiczny
+		    $this->soll = md5($seed.$this->walletAdres);
+	       // }
+	}
+}
